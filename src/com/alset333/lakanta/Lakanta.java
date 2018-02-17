6078c3797d294e2749f2d9623732b65c5146d79f
Peter Maar <alset333@icloud.com>
2018-02-16 21:20:47 -0500

    Setup Eclipse and Java Project

---
+++ src/com/alset333/lakanta/Lakanta.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.alset333.lakanta;
+
+/**
+ * @author alset333
+ *
+ */
+public class Lakanta {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
